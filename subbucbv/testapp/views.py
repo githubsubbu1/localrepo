3b353b1f811e61f98b04ae1aa33d7c25d44b032c
githubsubbu1 <subhanishaik.mb@gmail.com>
2020-05-05 22:06:58 -0700

    firstcommit

---
+++ subbucbv/testapp/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from testapp.models import Company
+from django.core.urlresolvers import reverse_lazy
+from django.views.generic import ListView,DetailView,CreateView,UpdateView,DeleteView
+
+class CompanyListView(ListView):
+    model=Company
+class CompanyDetailView(DetailView):
+    model=Company
+class CompanyCreateView(CreateView):
+    model=Company
+    fields=('name','location','ceo')
+class CompanyUpdateView(UpdateView):
+    model=Company
+    fields=('name','ceo')
+class CompanyDeleteView(DeleteView):
+    model=Company
+    success_url=reverse_lazy('companies')
